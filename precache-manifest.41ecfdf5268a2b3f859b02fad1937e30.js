3d09e0fee3d8da651efdac07eb49bc0c7396c3d1
darrenmuliawan <darrenm2@illinois.edu>
2020-08-02 13:57:24 -0700

    Updates

---
+++ precache-manifest.41ecfdf5268a2b3f859b02fad1937e30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57075386ec16d9106a04c4eb0876ebe0",
+    "url": "/OpenDota-Viz/index.html"
+  },
+  {
+    "revision": "631daae14be41b6a1e6f",
+    "url": "/OpenDota-Viz/static/css/main.18f2513a.chunk.css"
+  },
+  {
+    "revision": "46100e0a670441cca621",
+    "url": "/OpenDota-Viz/static/js/2.45612d49.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/OpenDota-Viz/static/js/2.45612d49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "631daae14be41b6a1e6f",
+    "url": "/OpenDota-Viz/static/js/main.326acfe8.chunk.js"
+  },
+  {
+    "revision": "bad85b560b013a3ff4b2",
+    "url": "/OpenDota-Viz/static/js/runtime-main.8e722b8d.js"
+  }
+]);
